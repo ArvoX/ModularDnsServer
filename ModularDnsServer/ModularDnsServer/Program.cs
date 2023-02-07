1d4361f0ebca5845e2f941a7d990b0fb14c6630c
ArvoX <git@arvox.dk>
2023-02-07 23:55:05 +0100

    Try to setup host

--- ModularDnsServer/ModularDnsServer/Program.cs
+++ ModularDnsServer/ModularDnsServer/Program.cs
@@ -1,8 +1,11 @@
-﻿using ModularDnsServer.Core;
+﻿using Microsoft.Extensions.Hosting;
+using ModularDnsServer.Core;
 using ModularDnsServer.Core.Dns.Cache;
 using ModularDnsServer.Resolvers.StaticResolvers;
 using System.Net;
 
+var builder = Host.CreateDefaultBuilder(args);
+using var host = builder.Build();
 var conf = new ServerConfiguration()
 {
   UpdPort = IPEndPoint.Parse("127.0.0.5:53")
@@ -10,3 +13,4 @@ var conf = new ServerConfiguration()
 var cache = new NullCache(new ConfigurableUpstreamResolver(IPEndPoint.Parse("8.8.8.8:53")));
 
 await new Server(conf, cache).RunAsync(CancellationToken.None);
+await host.RunAsync();
